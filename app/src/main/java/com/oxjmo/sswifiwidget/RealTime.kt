6745a0cbc4700a0d477470bce8a673a744c50947
_XJM_ <347867019@qq.com>
2025-07-03 23:27:56 +0800

    初步完成

--- app/src/main/java/com/oxjmo/sswifiwidget/RealTime.kt
+++ app/src/main/java/com/oxjmo/sswifiwidget/RealTime.kt
@@ -4,16 +4,16 @@ import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.util.Log
 import android.widget.RemoteViews
 import java.util.Calendar
 import java.text.SimpleDateFormat
 import java.util.Locale
 import android.content.Intent
-import android.content.ComponentName
-import androidx.core.net.toUri
+import android.widget.Toast
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -52,18 +52,17 @@ class RealTime : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetId: Int
     ) {
+        Toast.makeText(context, "等待刷新", Toast.LENGTH_SHORT).show()
         val views = RemoteViews(context.packageName, R.layout.real_time)
-
-        val calendar = Calendar.getInstance()
-        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-        val currentTime = sdf.format(calendar.time)
-        views.setTextViewText(R.id.infoTitle, currentTime)
-        views.setTextViewText(R.id.electricQuantity, "30%")
+        views.setTextViewText(R.id.ip, "--")
+        views.setTextViewText(R.id.rssi, "--dBm")
+        views.setTextViewText(R.id.electricQuantity, "--%")
         appWidgetManager.updateAppWidget(appWidgetId, views)
 
-
-        val refreshIntent = Intent(context, RealTime::class.java)
-        refreshIntent.action = ACTION_REFRESH
+        val refreshIntent = Intent(context, RealTime::class.java).apply {
+            action = ACTION_REFRESH
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+        }
         val refreshPendingIntent = PendingIntent.getBroadcast(
             context,
             appWidgetId,
@@ -72,28 +71,44 @@ class RealTime : AppWidgetProvider() {
         )
         views.setOnClickPendingIntent(R.id.widget_refresh, refreshPendingIntent)
 
+        val timestamp = System.currentTimeMillis()
         GlobalScope.launch {
-            val url = "http://cloud.oxjmo.top/test/test.json".toHttpUrl()
-            val client = OkHttpClient()
-            val request = Request.Builder().url(url).build()
-            val response: Response = client.newCall(request).execute()
-            val data = response.body?.string() ?: "Error: no date"
-            val jsonObject = JSONObject(data)
-            println("数据： $data")
-            println("电量： ${jsonObject.getString("battery")}")
-            views.setTextViewText(R.id.electricQuantity, jsonObject.getString("battery"))
+            delay(200L)
+            val homePageData = request("http://192.168.1.1/xml_action.cgi?method=get&module=duster&file=json_homepage_info$timestamp".toHttpUrl())
+            val statusData = request("http://192.168.1.1/xml_action.cgi?method=get&module=duster&file=json_status_info$timestamp".toHttpUrl())
+//            val simData = request("http://192.168.1.1/xml_action.cgi?method=get&module=duster&file=json_mss_support_info$timestamp".toHttpUrl())
+//            println(simData)
+            val ip = homePageData.getString("lan_ip")
+            val electricQuantity = statusData.getString("battery_percent")
+            val rssi = statusData.getString("rssi")
+            views.setTextViewText(R.id.ip, ip)
+            views.setTextViewText(R.id.rssi, "${rssi}dBm")
+            views.setTextViewText(R.id.electricQuantity, "$electricQuantity%")
+            val calendar = Calendar.getInstance()
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val currentTime = sdf.format(calendar.time)
+            views.setTextViewText(R.id.infoTitle, currentTime)
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
 
+    private fun request(url: HttpUrl): JSONObject {
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+        val response: Response = client.newCall(request).execute()
+        val data = response.body?.string() ?: "Error: no date"
+        return JSONObject(data)
+    }
+
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
-        val active = intent.action
-        if(active === ACTION_REFRESH) {
+        val action = intent.action
+        if (action == ACTION_REFRESH) {
             val appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
-            if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
-                println("检测到按钮点击，开始刷新小控件")
+            if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
                 updateAppWidget(context, AppWidgetManager.getInstance(context), appWidgetId)
+            } else {
+                println("警告：appWidgetId 无效")
             }
         }
     }
