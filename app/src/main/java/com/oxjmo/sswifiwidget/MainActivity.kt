241b30369c205c870b12378bc2d04475ca570af0
_XJM_ <347867019@qq.com>
2025-07-20 21:56:44 +0800

    主视图更新

--- app/src/main/java/com/oxjmo/sswifiwidget/MainActivity.kt
+++ app/src/main/java/com/oxjmo/sswifiwidget/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.oxjmo.sswifiwidget.ui.theme.SswifiWidgetTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +20,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             SswifiWidgetTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "请返回桌面添加桌面小控件"
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SswifiWidgetTheme {
-        Greeting("Android")
     }
 }
